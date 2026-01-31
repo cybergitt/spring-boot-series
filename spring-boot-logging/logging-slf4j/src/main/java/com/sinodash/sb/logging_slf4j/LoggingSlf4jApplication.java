7bc7a69f1cd510aae75a408700c228774d0843f9
Sigit Prayitno <cybergitt@gmail.com>
2026-01-31 21:36:39 +0700

    Add sample Logging SLF4J

---
+++ spring-boot-logging/logging-slf4j/src/main/java/com/sinodash/sb/logging_slf4j/LoggingSlf4jApplication.java
@@ -0,0 +1,13 @@
+package com.sinodash.sb.logging_slf4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingSlf4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingSlf4jApplication.class, args);
+	}
+
+}
---
+++ spring-boot-logging/logging-slf4j/src/test/java/com/sinodash/sb/logging_slf4j/LoggingSlf4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinodash.sb.logging_slf4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingSlf4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
