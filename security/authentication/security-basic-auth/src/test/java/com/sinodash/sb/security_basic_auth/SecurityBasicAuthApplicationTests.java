8dff4d4d64c0d976b2e91ab3ca5d111d456c22f6
Sigit Prayitno <cybergitt@gmail.com>
2026-03-14 23:43:11 +0700

    Init project

---
+++ security/authentication/security-basic-auth/src/test/java/com/sinodash/sb/security_basic_auth/SecurityBasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinodash.sb.security_basic_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityBasicAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
