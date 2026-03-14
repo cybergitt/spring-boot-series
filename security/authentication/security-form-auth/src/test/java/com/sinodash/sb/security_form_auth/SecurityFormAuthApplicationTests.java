b4b276f6da7dc19fe961cf28212f082ec68d5b57
Sigit Prayitno <cybergitt@gmail.com>
2026-03-15 00:31:50 +0700

    Init project

---
+++ security/authentication/security-form-auth/src/test/java/com/sinodash/sb/security_form_auth/SecurityFormAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinodash.sb.security_form_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityFormAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
