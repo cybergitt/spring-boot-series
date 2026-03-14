e57ed274d1dded8cbacec9f391e93520248900be
Sigit Prayitno <cybergitt@gmail.com>
2026-03-15 01:15:06 +0700

    Init project

---
+++ security/authentication/security-jwt-auth/src/main/java/com/sinodash/sb/security_jwt_auth/SecurityJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.sinodash.sb.security_jwt_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtAuthApplication.class, args);
+	}
+
+}
