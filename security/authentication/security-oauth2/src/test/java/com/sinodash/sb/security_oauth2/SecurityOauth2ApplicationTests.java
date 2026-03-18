9e39dde313b1261736f26d76f9dc0675bf82c23d
Sigit Prayitno <cybergitt@gmail.com>
2026-03-19 00:47:41 +0700

    Init project

---
+++ security/authentication/security-oauth2/src/test/java/com/sinodash/sb/security_oauth2/SecurityOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinodash.sb.security_oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
